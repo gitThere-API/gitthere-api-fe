a7c4cd5c6700c8e2cb21f872339b6ac49e12d2c5
Perry Sittser <sittserp@gmail.com>
2020-11-18 18:49:24 -0800

    header CSS

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './Header.css';
 
 export default class Header extends Component {
     render() {
@@ -8,23 +9,25 @@ export default class Header extends Component {
             <div className="nav">
 
                 {this.props.token
-                    ? <><div className="user">
-                        Hello, {this.props.username}
-                        <button onClick={this.props.logOut}>Log Out</button>
+                    ? <><div className="header-user">
+                        <div>Hello, {this.props.username}</div>
+                        <div><button onClick={this.props.logOut}>Log Out</button></div>
                     </div>
-                        <div className="nav-links">
-                            <Link to="/">| Home |</Link>
-                            <Link to="/map"> Map |</Link>
-                            <Link to="/about"> About |</Link>
+                        <div className="header-nav-links">
+                            <Link className="nav-link-button" to="/">Home</Link>
+                            <Link className="nav-link-button" to="/map">Map</Link>
+                            <Link className="nav-link-button" to="/about">About</Link>
                         </div>
                     </>
                     :
-                    <div>
-                        <Link to="/">| Home |</Link>
-                        <Link to="/about"> About |</Link>
+
+                    <div className="header-nav-links">
+                        <Link className="nav-link-button" to="/">Home</Link>
+                        <Link className="nav-link-button" to="/about">About</Link>
                     </div>
+
                 }
-                <div className="title">
+                <div className="header-title">
                     <h1>gitThere: API</h1>
                 </div>
 
