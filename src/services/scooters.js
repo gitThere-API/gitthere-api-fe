828a15da2657f55ec88ba0fcc3be1019ac4cf486
ErikGraciosa <erikgraciosa@gmail.com>
2021-05-17 23:23:21 -0700

    refactored services to switch statement

--- src/services/scooters.js
+++ src/services/scooters.js
@@ -2,8 +2,20 @@ import request from 'superagent';
 
 const URL = process.env.REACT_APP_URL;
 
-export const getLimeScooters = async (lat, lng, token) => {
-  return await request
+export const getScooters = async (lat, lng, token, brand) => {
+  switch(brand){
+    default: 
+    case 'lime':
+      return await request
             .get(`${URL}/api/lime?lat=${lat}&lon=${lng}`)
-            .set('Authorization', token)
+            .set('Authorization', token);
+    case 'nike':
+      return await request
+            .get(`${URL}/api/nike?lat=${lat}&lon=${lng}`)
+            .set('Authorization', token);   
+    case 'spin':
+      return await request
+            .get(`${URL}/api/spin?lat=${lat}&lon=${lng}`)
+            .set('Authorization', token);
+  }
 }
